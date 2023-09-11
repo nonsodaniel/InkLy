5e88c0e765c5da96ba9b1dcaac890a2f7de21b92
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-12 00:43:23 +0100

    Added responsiveness on home page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container">
+          <div className="wrapper">{children} </div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="">How are you</main>;
+  return (
+    <main className="">
+      <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas
+        laborum quasi fugiat dolore ducimus hic qui veritatis magnam
+        perspiciatis et, consequuntur rem facilis aliquam, saepe ratione iusto
+        est ea sed.
+      </h1>
+      <Link href={"/"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus rem illum
+        exercitationem, architecto placeat fuga sapiente quod praesentium
+        pariatur aperiam dolor adipisci nulla omnis, id accusantium labore
+        mollitia ducimus ut! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus rem illum exercitationem, architecto placeat fuga sapiente
+        quod praesentium pariatur aperiam dolor adipisci nulla omnis, id
+        accusantium labore mollitia ducimus ut! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus rem illum exercitationem, architecto
+        placeat fuga sapiente quod praesentium pariatur aperiam dolor adipisci
+        nulla omnis, id accusantium labore mollitia ducimus ut!
+      </Link>
+    </main>
+  );
 }
