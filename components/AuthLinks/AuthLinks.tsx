2ba9e51261c3f7b493826aad0eafc08aed3ead3f
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-12 21:08:02 +0100

    Added Auth component

---
+++ components/AuthLinks/AuthLinks.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+
+const AuthLinks = () => {
+  return (
+    <>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import AuthLinks from "../AuthLinks/AuthLinks";
 
 function Navbar() {
   return (
@@ -9,11 +11,12 @@ function Navbar() {
       <div className={styles.social}>
         <Image src="/facebook.png" alt="facebook" width={24} height={24} />
         <Image src="/instagram.png" alt="instagram" width={24} height={24} />
-        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>Inkly</div>
+      <div className={styles.logo}>lamablog</div>
       <div className={styles.links}>
+        <ThemeToggle />
         <Link href="/" className={styles.link}>
           Homepage
         </Link>
@@ -23,6 +26,7 @@ function Navbar() {
         <Link href="/" className={styles.link}>
           About
         </Link>
+        <AuthLinks />
       </div>
     </div>
   );
