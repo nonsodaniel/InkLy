25eaf84d3cc01c5c529fa564d5511f9b18d32886
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-12 20:32:30 +0100

    Created homepage component files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
+import Footer from "@/components/Footer/Footer";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Inkly",
   description: "A Blog application",
 };
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,9 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children} </div>
+          <div className="wrapper">
+            <Navbar />
+            {children} <Footer />
+          </div>
         </div>
       </body>
     </html>
---
+++ components/CardList/CardList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pagination from "../Pagination/Pagination";
+
+const CardList = () => {
+  return (
+    <div>
+      Card list
+      <Pagination />
+    </div>
+  );
+};
+
+export default CardList;
