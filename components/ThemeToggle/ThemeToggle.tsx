89af13253cd0da53fb1c0802f7fa9736649fb7a8
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-19 13:30:21 +0100

    Added toggle dark/light mode functionality

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -6,10 +6,10 @@ import { useContext } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  const { theme } = useContext(ThemeContext) ?? {};
+  const { theme, toggle } = useContext(ThemeContext) ?? {};
   console.log(theme);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src="/moon.png" alt="" width={14} height={14} />
       <div className={styles.ball}></div>
       <Image src="/sun.png" alt="" width={14} height={14} />
